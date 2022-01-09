8eee1d130b188bc61d23a6ae167a139534fef62a
Саша <alexandr@MacBook-Pro-Sasa.local>
2022-01-10 01:49:43 +0300

    update

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, TextInput, View } from "react-native";
+import Container from "../../components/common/Container";
 
 const Login = () => {
+  const [text, onChangeText] = React.useState("Useless Text");
 
-    return (
-
-        <View>
-            <Text>Hi from Login</Text>
-        </View>
-    );
+  return (
+    <Container>
+      <Text>Hi from Login</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </Container>
+  );
 };
 
 export default Login;
